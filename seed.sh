135464afafe472b8aeae639890fd8823944aab7e
eddiebarry <edgarmonis@gmail.com>
2021-06-07 12:15:26 +0530

    :wrench: automated load test generation :wrench:

--- seed.sh
+++ seed.sh
@@ -2,27 +2,38 @@
 
 testFile=""
 hostName=""
+dcMasterName=""
+dcSlaveName=""
+scriptName=""
 
 # Display prompt if not arguments passed
-if [[ -z "$1" && -z "$2" ]]
+if [[ -z "$1" && -z "$2" && -z "$3" && -z "$4" && -z "$5"  && -z "$6" ]]
 then
   read -p 'File to run: ' testFile
   read -p 'Host to attack: ' hostName
+  read -p 'Name of master service: ' dcmastername
+  read -p 'Host to worker service: ' dcslavename
+  read -p 'Name of the script: ' scriptName
+  read -p 'Name of the host url: ' hostUrlName
 else
   testFile=$1
   hostName=$2
+  dcMasterName=$3
+  dcSlaveName=$4
+  scriptName=$5
+  hostUrlName=$6
 fi
 
 # Confirmation
-echo "Confirmation: file to run is: $testFile and the host: $hostName"
+echo "Confirmation: file to run is: $testFile, the host: $hostName and the script name is $scriptName"
 
 # Prepare Config map with new values
 cat > config-map.yaml << EOF1
 kind: ConfigMap
 apiVersion: v1
 metadata:
-  name: host-url
-  namespace: locust
+  name: $hostUrlName
+  namespace: labs-dev
 data:
   ATTACKED_HOST: $hostName
 EOF1
@@ -38,12 +49,14 @@ cat > config-map.yaml << EOF1
 kind: ConfigMap
 apiVersion: v1
 metadata:
-  name: script-file
-  namespace: locust
+  name: $scriptName
+  namespace: labs-dev
 data:
   locustfile.py: |
 $(cat $testFile | sed 's/^/    /')
 EOF1
+# oldName='script-file'
+# sed -i '' "s/${oldName}/${scriptName}/g" config-map.yaml
 
 # Push it to cluster
 cat config-map.yaml | oc apply -f -
@@ -52,11 +65,11 @@ cat config-map.yaml | oc apply -f -
 rm ./config-map.yaml
 
 # Update the environment variable to trigger a change
-oc project locust
+oc project labs-dev
 #oc set env dc/locust-master --overwrite CONFIG_HASH=`date +%s%N`
 #oc set env dc/locust-slave --overwrite CONFIG_HASH=`date +%s%N`
 
-confighash=`date +%s%N`
+# confighash=`date +%s%N`
 
-oc set env dc/locust-master --overwrite CONFIG_HASH=$confighash
-oc set env dc/locust-slave --overwrite CONFIG_HASH=$confighash
+# oc set env dc/${dcMasterName} --overwrite CONFIG_HASH=$confighash
+# oc set env dc/${dcSlaveName} --overwrite CONFIG_HASH=$confighash
