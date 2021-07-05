15c3ad7ba34a713734e852cc9eb0a6a4a8b11f55
eddiebarry <edgarmonis@gmail.com>
2021-07-05 16:32:25 +0530

    :wrench: adding windows support :wrench:

--- setup_load_tests.sh
+++ setup_load_tests.sh
@@ -19,22 +19,46 @@ for fileName in ./*.py; do
     onlyNamemaster="$onlyName$master"
     onlyNameslave="$onlyName$slave"
     oldName='service_name'
-    sed -i '' "s/${oldName}/${onlyNamemaster}/g" $newName
+
+    # sed -i '' "s/${oldName}/${onlyNamemaster}/g" $newName
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${onlyNamemaster}/g" $newName
+    else
+        sed -i -e "s/${oldName}/${onlyNamemaster}/g" $newName
+    fi
+
+    
     
     oldName='script-file'
     scriptName="$oldName-$onlyName"
-    sed -i '' "s/${oldName}/${scriptName}/g" $newName
+
+    # sed -i '' "s/${oldName}/${scriptName}/g" $newName
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${scriptName}/g" $newName
+    else
+        sed -i -e "s/${oldName}/${scriptName}/g" $newName
+    fi  
     
     oldName='host-url'
     hostUrlName="$oldName-$onlyName"
-    sed -i '' "s/${oldName}/${hostUrlName}/g" $newName
+    # sed -i '' "s/${oldName}/${hostUrlName}/g" $newName
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${hostUrlName}/g" $newName
+    else
+        sed -i -e "s/${oldName}/${hostUrlName}/g" $newName
+    fi  
 
     # create copies of slave deployment
     tempfileName='slave-deployment-_.yaml'
     newNameSlave=${tempfileName/'_'/$onlyName}
     cp ./slave-deployment.yaml $newNameSlave
     oldName='service_name'
-    sed -i '' "s/${oldName}/${onlyNameslave}/g" $newNameSlave
+    # sed -i '' "s/${oldName}/${onlyNameslave}/g" $newNameSlave
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${onlyNameslave}/g" $newNameSlave
+    else
+        sed -i -e "s/${oldName}/${onlyNameslave}/g" $newNameSlave
+    fi  
 
     oldName='LOCUST_SERVICE_HOST'
     hostAppend="_PORT_8089_TCP_ADDR"
@@ -43,14 +67,32 @@ for fileName in ./*.py; do
     newHost=${newHost/'-'/'_'}
     newHost=${newHost/'-'/'_'}
     newHost=`echo "${newHost}" | tr '[a-z]' '[A-Z]'`
-    sed -i '' "s/${oldName}/${newHost}/g" $newNameSlave
+
+    # sed -i '' "s/${oldName}/${newHost}/g" $newNameSlave
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${newHost}/g" $newNameSlave
+    else
+        sed -i -e "s/${oldName}/${newHost}/g" $newNameSlave
+    fi  
     
     oldName='script-file'
     scriptName="$oldName-$onlyName"
-    sed -i '' "s/${oldName}/${scriptName}/g" $newNameSlave
+
+    # sed -i '' "s/${oldName}/${scriptName}/g" $newNameSlave
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${scriptName}/g" $newNameSlave
+    else
+        sed -i -e "s/${oldName}/${scriptName}/g" $newNameSlave
+    fi 
     
     oldName='host-url'
-    sed -i '' "s/${oldName}/${hostUrlName}/g" $newNameSlave
+    # sed -i '' "s/${oldName}/${hostUrlName}/g" $newNameSlave
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e "s/${oldName}/${hostUrlName}/g" $newNameSlave
+    else
+        sed -i -e "s/${oldName}/${hostUrlName}/g" $newNameSlave
+    fi 
+    
     
     # first create config variables
     sh ./seed.sh $fileName $tempHost $onlyNamemaster $onlyNameslave $scriptName $hostUrlName
